8a63e9564587a94f3455d731a4e7eb5ba0d16f2e
David Luo <davidkluo@gmail.com>
2023-05-10 18:06:21 -0700

    Add logo and welcome message to root page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div
@@ -12,7 +14,40 @@ export default function Home() {
         bg-gray-100
       "
     >
-      {"David's"} Messenger Clone
+      <div className="sm:mx-auto sm:w-full sm-max-w-md">
+        <Image
+          alt="Facebook Messenger Logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2
+          className="
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Welcome to Messenger
+        </h2>
+        <h3
+          className="
+            mt-2
+            text-center
+            text-xl
+            tracking-tight
+            text-gray-900
+          "
+        >
+          Let&apos;s get chatting!
+        </h3>
+      </div>
+
+      {/* Auth form here */}
     </div>
   );
 }
