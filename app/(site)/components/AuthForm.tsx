d5f4c8c493ea13714c0d4dff6a088d6a42a7a61e
David Luo <davidkluo@gmail.com>
2023-05-10 22:41:58 -0700

    Create and add Button component to AuthForm

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -62,9 +63,8 @@ function AuthForm() {
         sm:max-w-md
       "
     >
-      <form
+      <div
         className="
-          space-y-6
           bg-white
           px-4
           py-8
@@ -72,43 +72,71 @@ function AuthForm() {
           sm:rounded-lg
           sm:px-10
         "
-        onSubmit={handleSubmit(onSubmit)}
       >
-        {variant === "REGISTER" && (
-          <>
-            <Input
-              id="first-name"
-              label="First Name"
-              register={register}
-              errors={errors}
-              required={true}
-            />
-            <Input
-              id="last-name"
-              label="Last Name"
-              register={register}
-              errors={errors}
-              required={true}
-            />
-          </>
-        )}
-        <Input
-          id="email"
-          label="Email Address"
-          register={register}
-          errors={errors}
-          type="email"
-          required={true}
-        />
-        <Input
-          id="password"
-          label="Password"
-          register={register}
-          errors={errors}
-          type="password"
-          required={true}
-        />
-      </form>
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <>
+              <Input
+                id="first-name"
+                label="First Name"
+                register={register}
+                errors={errors}
+                required={true}
+              />
+              <Input
+                id="last-name"
+                label="Last Name"
+                register={register}
+                errors={errors}
+                required={true}
+              />
+            </>
+          )}
+          <Input
+            id="email"
+            label="Email Address"
+            register={register}
+            errors={errors}
+            type="email"
+            required={true}
+          />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            errors={errors}
+            type="password"
+            required={true}
+          />
+          <div>
+            <Button type="submit" disabled={isLoading} fullWidth>
+              {variant === "LOGIN" ? "Log in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+              absolute
+              inset-0
+              flex
+              items-center
+            "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">Auth Social Buttons</div>
+        </div>
+      </div>
     </div>
   );
 }
