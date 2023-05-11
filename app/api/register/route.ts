3b63ede8e0bfefdcaf5fa3cb030360f7ca961af8
David Luo <davidkluo@gmail.com>
2023-05-11 10:58:00 -0700

    Create register api route

---
+++ app/api/register/route.ts
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { firstName, lastName, email, password } = body;
+
+    if (!firstName || !lastName || !email || !password) {
+      return new NextResponse("Missing fields", { status: 400 });
+    }
+
+    // TODO: Create work factor environment variable
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log("REGISTRATION_ERROR", error);
+
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
