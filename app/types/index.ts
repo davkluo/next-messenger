37b2a77989a8f90cc327be16d8ae6bbfbd4ac2b8
David Luo <davidkluo@gmail.com>
2023-05-11 21:21:13 -0700

    Create types for fully nested conversation record

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import { User, Conversation, Message } from "@prisma/client";
+
+export type FullMessage = Message & {
+  sender: User;
+  seen: User[];
+};
+
+export type FullConversation = Conversation & {
+  users: User[];
+  messages: FullMessage[];
+};
