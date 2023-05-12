47cd427753ce3ed6d70fb0a3e3607d45484c565c
David Luo <davidkluo@gmail.com>
2023-05-11 17:38:05 -0700

    Create current user avatar on sidebar and people page

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,10 +2,17 @@
 
 import { useState } from "react";
 
+import { User } from "@prisma/client";
+
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
+import Avatar from "../Avatar";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User | null;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -56,6 +63,26 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav
+        className="
+          mt-4
+          flex
+          flex-col
+          justify-between
+          items-center
+        "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+            cursor-pointer
+            hover:opacity-75
+            transition
+          "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
