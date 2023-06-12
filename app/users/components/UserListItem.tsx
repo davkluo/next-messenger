3a64dcff85e38086e8b0e687964cf66b6bdd4407
David Luo <davidkluo@gmail.com>
2023-06-11 20:27:20 -0700

    Apply loading modal to UserListItem

--- app/users/components/UserListItem.tsx
+++ app/users/components/UserListItem.tsx
@@ -5,7 +5,9 @@ import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 
 import { User } from "@prisma/client";
+
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 
 interface UserListItemProps {
   user: User;
@@ -27,33 +29,36 @@ const UserListItem: React.FC<UserListItemProps> = ({ user }) => {
   }, [user, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
-        w-full
-        relative
-        flex
-        items-center
-        space-x-3
-        bg-white
-        p-3
-        hover:bg-neutral-100
-        rounded-lg
-        transition
-        cursor-pointer
-      "
-    >
-      <Avatar user={user} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">
-              {`${user.firstName} ${user.lastName}`}
-            </p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="
+          w-full
+          relative
+          flex
+          items-center
+          space-x-3
+          bg-white
+          p-3
+          hover:bg-neutral-100
+          rounded-lg
+          transition
+          cursor-pointer
+        "
+      >
+        <Avatar user={user} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">
+                {`${user.firstName} ${user.lastName}`}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
